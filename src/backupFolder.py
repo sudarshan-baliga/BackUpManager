ad50609da85b08a7aa4531b86562ef804bd75e68
sudarshan <baliga108@gmail.com>
2018-12-30 20:29:03 +0530

    implemented creation of symlinks when same file exists

--- src/backupFolder.py
+++ src/backupFolder.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import pymysql
 
+
 class Backupfolder():
     def __init__(self):
         self.files = []
@@ -12,23 +13,32 @@ class Backupfolder():
         directories = [x[0] for x in os.walk(fullpath)]
         for directory in directories:
             for file in os.listdir(directory):
-                absFile = os.path.join(directory , file)
+                absFile = os.path.join(directory, file)
                 if os.path.isfile(absFile):
                     self.files.append(absFile)
-    
 
     def copyfile(self, filepath):
         try:
             path, file = os.path.split(filepath)
             os.makedirs(os.path.join("backup", path), exist_ok=True)
-            shutil.copy2(filepath, os.path.join("backup", filepath)) 
+            shutil.copy2(filepath, os.path.join("backup", filepath))
             return True
         except:
             print("could not copy {} into backup folder".format(filepath))
         return False
 
     def createsymlink(self, src, dest):
-        pass
+        """create symlink of src pointing to dest."""
+        print(src, dest)
+        try:
+            path, file = os.path.split(dest)
+            os.makedirs(path, exist_ok=True)
+            # src should be absolute path
+            print(file)
+            os.symlink(src, dest)
+        except Exception as e:
+            print("could not create symlink", dest)
+            print(e.__doc__)
 
     def backup(self, fullpath):
         """Backup everthing present in the path."""
@@ -48,13 +58,20 @@ class Backupfolder():
                 print("not able to query database to check the hashes")
             if cursor.rowcount == 0:
                 res = self.copyfile(file)
-                if(res):
+                if res:
                     try:
-                        query = "INSERT into hashes values(\'{}\',\'{}\')".format(hash, file)
+                        destpath = os.path.join(os.path.abspath("backup"), file)
+                        query = "INSERT into hashes values(\'{}\',\'{}\')".format(hash, destpath)
                         cursor.execute(query)
                         db.commit()                   
                     except:
                         db.rollback()
                         print("could no insert {} into data base".format(file))
             else:
-                print(file , " already exists create symlink")
+                if os.path.exists(os.path.join("backup", file)):
+                    print("no changes made for", file)
+                else:
+                    srcdata = cursor.fetchone()
+                    src = srcdata[1]
+                    dest = os.path.join("backup", file)
+                    self.createsymlink(src, dest)
